f82d8458d246ba136a7bd90a2307e827b223d86b
timo9036 <timo9036@hotmail.com>
2025-09-23 03:49:00 -0500

    set up room entity

---
+++ app/src/main/java/com/example/todo_compose/data/models/Priority.kt
@@ -0,0 +1,14 @@
+package com.example.todo_compose.data.models
+
+import androidx.compose.ui.graphics.Color
+import com.example.todo_compose.ui.theme.HighPriorityColor
+import com.example.todo_compose.ui.theme.LowPriorityColor
+import com.example.todo_compose.ui.theme.MediumPriorityColor
+
+enum class Priority( val color: Color) {
+
+    HIGH(HighPriorityColor),
+    MEDIUM(MediumPriorityColor),
+    LOW(LowPriorityColor),
+    NONE(Color.Transparent)
+}
---
+++ app/src/main/java/com/example/todo_compose/util/Constants.kt
@@ -0,0 +1,13 @@
+package com.example.todo_compose.util
+
+object Constants {
+
+    const val DATABASE_TABLE = "todo_table"
+    const val DATABASE_NAME = "todo_database"
+
+    const val PREFERENCE_NAME = "todo_preferences"
+    const val PREFERENCE_KEY = "sort_state"
+
+    const val MAX_TITLE_LENGTH = 20
+
+}
